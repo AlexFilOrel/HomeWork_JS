9e26ed3d2ef7659df719a4c190e700df34593208
AlexFilOrel <orel-alex@ya.ru>
2023-02-04 11:48:57 +0300

    Ready HW 1

---
+++ HW_1/script.js
@@ -0,0 +1,12 @@
+// Task 1
+
+let degC = 20;
+let degF = degC * 9 / 5 + 32;
+console.log(`Температура ${degC}°C равна температуре ${degF}°F.`);
+
+
+// Task 2
+
+let name = 'Alexander';
+let admin = name;
+console.log(admin);
